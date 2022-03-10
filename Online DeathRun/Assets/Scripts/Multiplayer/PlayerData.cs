7a0f271c958e3d3478af60da1d6cf8441b542f1e
Francesco Tamburini <70216190+Francesco-Tamburini@users.noreply.github.com>
2022-03-10 21:54:44 +0100

    Ho provato a fare il socket ma non va :(

---
+++ Online DeathRun/Assets/Scripts/Multiplayer/PlayerData.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable]
+public struct PlayerData
+{
+    public string id;
+    public float xPos;
+    public float yPos;
+    public float zPos;
+    public double timestamp;
+}
+
---
+++ Online DeathRun/Assets/Scripts/Multiplayer/Socket.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using Newtonsoft.Json.Linq;
+
+public class Socket : MonoBehaviour
+{
+    WebSocket socket;
+    public GameObject player;
+    public PlayerData playerData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        socket = new WebSocket("ws://localhost:8080");
+        //socket = new WebSocket("ws://websocket-starter-code-multiplayer-websocket-app.bsh-serverconnect-b3c-4x1-162e406f043e20da9b0ef0731954a894-0000.us-south.containers.appdomain.cloud/");
+        socket.Connect();
+
+        //WebSocket onMessage function
+        socket.OnMessage += (sender, e) =>
+        {
+
+            //If received data is type text...
+            if (e.IsText)
+            {
+                //Debug.Log("IsText");
+                //Debug.Log(e.Data);
+                JObject jsonObj = JObject.Parse(e.Data);
+
+                //Get Initial Data server ID data (From intial serverhandshake
+                if (jsonObj["id"] != null)
+                {
+                    //Convert Intial player data Json (from server) to Player data object
+                    PlayerData tempPlayerData = JsonUtility.FromJson<PlayerData>(e.Data);
+                    playerData = tempPlayerData;
+                    Debug.Log("player ID is " + playerData.id);
+                    return;
+                }
+
+            }
+
+        };
+
+        //If server connection closes (not client originated)
+        socket.OnClose += (sender, e) =>
+        {
+            Debug.Log(e.Code);
+            Debug.Log(e.Reason);
+            Debug.Log("Connection Closed!");
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(player.transform.position);
+        if (socket == null)
+        {
+            return;
+        }
+
+        //If player is correctly configured, begin sending player data to server
+        if (player != null && playerData.id != "")
+        {
+            //Grab player current position and rotation data
+            playerData.xPos = player.transform.position.x;
+            playerData.yPos = player.transform.position.y;
+            playerData.zPos = player.transform.position.z;
+
+            System.DateTime epochStart = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
+            double timestamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
+            //Debug.Log(timestamp);
+            playerData.timestamp = timestamp;
+
+            string playerDataJSON = JsonUtility.ToJson(playerData);
+            socket.Send(playerDataJSON);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            string messageJSON = "{\"message\": \"Some Message From Client\"}";
+            socket.Send(messageJSON);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //Close socket when exiting application
+        socket.Close();
+    }
+
+}
