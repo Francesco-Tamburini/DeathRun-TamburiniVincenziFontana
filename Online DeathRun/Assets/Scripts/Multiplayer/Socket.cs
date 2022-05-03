7c6328a86ccf878f289b7fa535f70f5bce1a3926
Francesco Tamburini <70216190+Francesco-Tamburini@users.noreply.github.com>
2022-05-03 08:59:50 +0200

    Update Socket.cs

--- Online DeathRun/Assets/Scripts/Multiplayer/Socket.cs
+++ Online DeathRun/Assets/Scripts/Multiplayer/Socket.cs
@@ -15,24 +15,20 @@ public class Socket : MonoBehaviour
     {
 
         socket = new WebSocket("ws://deathrun-server.glitch.me");
-        //socket = new WebSocket("ws://websocket-starter-code-multiplayer-websocket-app.bsh-serverconnect-b3c-4x1-162e406f043e20da9b0ef0731954a894-0000.us-south.containers.appdomain.cloud/");
         socket.Connect();
 
-        //WebSocket onMessage function
         socket.OnMessage += (sender, e) =>
         {
 
             //If received data is type text...
             if (e.IsText)
             {
-                //Debug.Log("IsText");
-                //Debug.Log(e.Data);
                 JObject jsonObj = JObject.Parse(e.Data);
-
-                //Get Initial Data server ID data (From intial serverhandshake
+                
+                //ottenimento id del server dati
                 if (jsonObj["id"] != null)
                 {
-                    //Convert Intial player data Json (from server) to Player data object
+                    //conversione dati player iniziali da json a PlayerData object
                     PlayerData tempPlayerData = JsonUtility.FromJson<PlayerData>(e.Data);
                     playerData = tempPlayerData;
                     Debug.Log("player ID is " + playerData.id);
@@ -43,7 +39,7 @@ public class Socket : MonoBehaviour
 
         };
 
-        //If server connection closes (not client originated)
+        //quando la connessione si chiude
         socket.OnClose += (sender, e) =>
         {
             Debug.Log(e.Code);
@@ -55,23 +51,21 @@ public class Socket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(player.transform.position);
         if (socket == null)
         {
             return;
         }
 
-        //If player is correctly configured, begin sending player data to server
+        //se il player è stato caricato correttamente inizio a inviare i dati al server
         if (player != null && playerData.id != "")
         {
-            //Grab player current position and rotation data
+            //Prendo la posizione del player
             playerData.xPos = player.transform.position.x;
             playerData.yPos = player.transform.position.y;
             playerData.zPos = player.transform.position.z;
 
             System.DateTime epochStart = new System.DateTime(1970, 1, 1, 8, 0, 0, System.DateTimeKind.Utc);
             double timestamp = (System.DateTime.UtcNow - epochStart).TotalSeconds;
-            //Debug.Log(timestamp);
             playerData.timestamp = timestamp;
 
             string playerDataJSON = JsonUtility.ToJson(playerData);
@@ -87,7 +81,7 @@ public class Socket : MonoBehaviour
 
     private void OnDestroy()
     {
-        //Close socket when exiting application
+        //chiusura del socket
         socket.Close();
     }
 
